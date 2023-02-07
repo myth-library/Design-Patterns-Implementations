6c11c7c59dce8cf96550207e9273d435a123a8f6
Ashen <ashenj.official@gmail.com>
2023-02-07 09:54:10 +0530

    Structural-Facade - Solution

--- src/design_patterns/Main.java
+++ src/design_patterns/Main.java
@@ -2,16 +2,11 @@ package design_patterns;
 
 import design_patterns.structural_patterns.facade.Message;
 import design_patterns.structural_patterns.facade.NotificationServer;
+import design_patterns.structural_patterns.facade.NotificationService;
 
 public class Main {
     public static void main(String[] args) {
-        /* Userට push notification යවන්න ඕනෙ වෙන හැම වෙලාවකදීම පහත steps
-           ඔක්කොම නැවත නැවත භාවිතා කිරීමට සිදු වේ */
-        var server = new NotificationServer();
-        var connection = server.connect("ip");
-        var authToken = server.authenticate("appID", "key");
-        var message = new Message("Hello World");
-        server.send(authToken, message, "target");
-        connection.disconnect();
+        var service = new NotificationService();
+        service.send("Hello World", "target");
     }
 }
