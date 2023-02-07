346c72633c27af95a4d940047a494a704c7abfed
Ashen <ashenj.official@gmail.com>
2023-02-07 07:08:40 +0530

    Behavioural-Memento - Solution

--- src/design_patterns/Main.java
+++ src/design_patterns/Main.java
@@ -1,15 +1,24 @@
 package design_patterns;
 
 import design_patterns.behavioural_patterns.memento.Editor;
+import design_patterns.behavioural_patterns.memento.History;
 
 public class Main {
     public static void main(String[] args) {
         var editor = new Editor();
+        var history = new History();
 
-        // How to implement following scenario
         editor.setContent("a");
+        history.push(editor.createState());
+
         editor.setContent("b");
+        history.push(editor.createState());
+
         editor.setContent("c");
-        editor.undo();
+
+        editor.restore(history.pop());
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
     }
 }
--- src/design_patterns/behavioural_patterns/memento/Editor.java
+++ src/design_patterns/behavioural_patterns/memento/Editor.java
@@ -3,6 +3,14 @@ package design_patterns.behavioural_patterns.memento;
 public class Editor {
     private String content;
 
+    public EditorState createState() {
+        return new EditorState(content);
+    }
+
+    public void restore(EditorState state) {
+        content = state.getContent();
+    }
+
     public String getContent() {
         return content;
     }
